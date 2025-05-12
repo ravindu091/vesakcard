3c1a82d0a5a86b74836ea7fef164f2314b6abf05
ravindu091 <diluparavindu091@gmil.com>
2025-05-12 10:00:53 +0530

    first

---
+++ components/VesakCard.tsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import { Card, CardHeader } from './ui/card'
+import Image from 'next/image'
+
+function VesakCard() {
+  return (
+    <Card className='w-[300px] h-90 p-0'>
+        
+            <Image 
+                src="/card/img1.png"
+                alt='Vesak card picture'
+                width={300}
+                height={400}
+                />
+        
+    </Card>
+  )
+}
+
+export default VesakCard
